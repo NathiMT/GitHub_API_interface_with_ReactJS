f1c7cba5c641b8ba262aa522b4a669361946cd3d
Nathália Menegol Teles <106335669+NathiMT@users.noreply.github.com>
2022-07-12 16:47:51 -0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,3 @@
+#GitHub API interface with ReactJS
+
+This project was based on O Matheus Benites' classes, for the DIO ReactJS challenge!
---
+++ src/components/layout/styled.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const WrapperLayout = styled.section`
+    margin: 10px;
+    padding: 10px;
+    border-radius: 8px;
+    background-color: #b5a5c4;
+`;
